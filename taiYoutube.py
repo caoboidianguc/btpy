11545486d6e98081d2ac9a52a213d69db32b6042
caoboidianguc <69663074+caoboidianguc@users.noreply.github.com>
2025-11-20 21:29:02 -0500

    chuyen doi thanh mp3
    Added functionality to convert downloaded MP4 videos to MP3 format using moviepy. Updated the video download process to remove the original video file after conversion.

--- taiYoutube.py
+++ taiYoutube.py
@@ -1,41 +1,49 @@
-
 from pytubefix import YouTube
 from pytubefix import Playlist
 from pytubefix.exceptions import VideoUnavailable
 from pytubefix.cli import on_progress, display_progress_bar
+from moviepy import VideoFileClip
+import os
 
-# pip install pytubefix
-
+luuVideo = 'C:\\Users\\hibis\\Downloads\\nhac'
+def mp4_to_mp3(duongdan_video, duongdan_audio):
+    video = VideoFileClip(duongdan_video)
+    file_name = os.path.basename(duongdan_video)
+    title = os.path.splitext(file_name)[0]
+    duongdan_audio = os.path.join(duongdan_audio, title + ".mp3")
+    video.audio.write_audiofile(duongdan_audio, codec='libmp3lame', bitrate='320k', fps=44100, logger=None)
+    video.close()
+    
 def taiVideos(duongdan):
     try:
-        yt = YouTube(duongdan, on_progress_callback = on_progress, on_complete_callback= display_progress_bar)
+        yt = YouTube(duongdan, on_progress_callback=on_progress)
     except VideoUnavailable as e:
         print(e)
         print('Video khong the down')
     else:
-        # yt.streams.get_audio_only()
         yt = yt.streams.get_highest_resolution()
         print(f"Download...{yt.title} ")
-        yt.download('C:\\Users\\hibis\\Downloads\\nhac')
-        # yt.download(mp3=True)
+        video = yt.download(luuVideo)
+        mp4_to_mp3(video, luuVideo)
+        os.remove(video)
         print(f"--{yt.title} --> Xong!")
-    
-    
-
 
 def taiList():
-    duongdan = input("Link List : ")
-    if duongdan == "":
+    duongLink = input("Link List : ").strip()
+    if not duongLink:
+        print("Khong co link")
         return
-    ds = Playlist(duongdan)
-    for video in ds.videos:
-        taiVideos(video)
+    if duongLink == "":
+        return
+    ds = Playlist(duongLink)
+    for url in ds.video_urls:
+        taiVideos(url)
                 
 
 
 def layLink():
     while True:
-        trang = input('Link video: ')
+        trang = input('Link video: ').strip()
         if trang == '': break
         elif trang == 'l':
             taiList()
