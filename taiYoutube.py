35ea09a800a85947354aebe1d6ed56d5d9b738cf
caoboidianguc <69663074+caoboidianguc@users.noreply.github.com>
2025-11-21 20:53:52 -0500

    Convert taiVideos and taiList to async functions
    Refactor taiVideos and taiList functions to use asyncio for concurrent downloads.

--- taiYoutube.py
+++ taiYoutube.py
@@ -1,11 +1,14 @@
 from pytubefix import YouTube
 from pytubefix import Playlist
 from pytubefix.exceptions import VideoUnavailable
-from pytubefix.cli import on_progress, display_progress_bar
+from pytubefix.cli import on_progress
 from moviepy import VideoFileClip
 import os
+import asyncio
+import concurrent.futures
 
-luuVideo = 'C:\\Users\\hibis\\Downloads\\nhac'
+luuVideo = r'C:/Users/hibis/Downloads/nhac'
+
 def mp4_to_mp3(duongdan_video, duongdan_audio):
     video = VideoFileClip(duongdan_video)
     file_name = os.path.basename(duongdan_video)
@@ -13,42 +16,43 @@ def mp4_to_mp3(duongdan_video, duongdan_audio):
     duongdan_audio = os.path.join(duongdan_audio, title + ".mp3")
     video.audio.write_audiofile(duongdan_audio, codec='libmp3lame', bitrate='320k', fps=44100, logger=None)
     video.close()
+
     
-def taiVideos(duongdan):
-    try:
-        yt = YouTube(duongdan, on_progress_callback=on_progress)
-    except VideoUnavailable as e:
-        print(e)
-        print('Video khong the down')
-    else:
-        yt = yt.streams.get_highest_resolution()
-        print(f"Download...{yt.title} ")
-        video = yt.download(luuVideo)
-        mp4_to_mp3(video, luuVideo)
-        os.remove(video)
-        print(f"--{yt.title} --> Xong!")
+async def taiVideos(duongdan, executor) -> None:
+    def async_tai():
+        try:
+            yt = YouTube(duongdan, on_progress_callback=on_progress)
+        except VideoUnavailable as e:
+            print(e)
+            print('Video khong the down')
+        else:
+            yt = yt.streams.get_highest_resolution()
+            print(f"Download...{yt.title} ")
+            video = yt.download(luuVideo)
+            mp4_to_mp3(video, luuVideo)
+            os.remove(video)
+            print(f"--{yt.title} --> Xong!")
+    await asyncio.get_event_loop().run_in_executor(executor, async_tai)
 
-def taiList():
-    duongLink = input("Link List : ").strip()
-    if not duongLink:
-        print("Khong co link")
-        return
-    if duongLink == "":
-        return
-    ds = Playlist(duongLink)
-    for url in ds.video_urls:
-        taiVideos(url)
+async def taiList(trang, max_concurrent=4) -> None:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent) as executor:
+        task = [
+            taiVideos(video_url, executor)
+            for video_url in Playlist(trang).video_urls
+        ]
+        await asyncio.gather(*task)
                 
-
 
 def layLink():
     while True:
         trang = input('Link video: ').strip()
         if trang == '': break
         elif trang == 'l':
-            taiList()
+            url_list = input('Link list video: ').strip()
+            asyncio.run(taiList(trang=url_list))
         else:
-            taiVideos(duongdan=trang)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+                asyncio.run(taiVideos(duongdan=trang, executor=executor))
 
 
 if __name__ == "__main__":
